6267a07351c3bf2240bfd098203c6bb1140965ff
viniciusaa <vini89go@gmail.com>
2021-02-19 07:18:54 +0000

    Updated schema, added order attribute to categories

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_231704) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.integer "order"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
