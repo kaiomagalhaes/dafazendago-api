14e401f069f2ee0e0684da02686cb7f5c2be5f4f
viniciusaa <vini89go@gmail.com>
2021-02-24 04:10:16 +0000

    Added 'category order' in the 'products' controller json, updated seeds, categories now are created with a order number and added more products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-c1 = Category.create(name: "Doces")
-c2 = Category.create(name: "Queijos")
-c3 = Category.create(name: "Carnes")
+c1 = Category.create(name: "Doces", order: 1)
+c2 = Category.create(name: "Queijos", order: 2)
+c3 = Category.create(name: "Carnes", order: 3)
 
 Product.create(name: "Doce de leite", category: c1, price: 3000)
 Product.create(name: "Doce de banana", category: c1, price: 3000)
 Product.create(name: "Doce de goiaba", category: c1, price: 3000)
 Product.create(name: "Doce de amora", category: c1, price: 3000)
 
-Product.create(name: "Queijo fresco", category: c1, price: 3000)
-Product.create(name: "Queijo minas", category: c1, price: 3000)
-Product.create(name: "Queijo no vinho", category: c1, price: 3000)
+Product.create(name: "Queijo fresco", category: c2, price: 3000)
+Product.create(name: "Queijo minas", category: c2, price: 3000)
+Product.create(name: "Queijo no vinho", category: c2, price: 3000)
+
+Product.create(name: "Lombo suíno defumado", category: c3, price: 3000)
+Product.create(name: "Bacon", category: c3, price: 3000)
+Product.create(name: "Rosbife bovino defumado", category: c3, price: 3000)
