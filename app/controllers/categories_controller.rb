2fc394795bb9ab3d7f7ccca74cb317a96e3d5713
viniciusaa <vini89go@gmail.com>
2021-02-24 04:24:18 +0000

    Updated 'products' controller removing minor writing mistake, created 'categories' controller with index action rendering all categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+    def index
+      categories = Category.all
+      json = categories.map do |category|
+        {
+          name: category.name,
+          order: category.order
+        }
+      end
+  
+      render json: json
+    end
+  end
+  
