275c6f7664390c8d16f9c41fdf33fc0bf508f83a
viniciusaa <vini89go@gmail.com>
2021-02-24 04:32:15 +0000

    Updated seeds to follow rubocop best practices

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,19 +8,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-c1 = Category.create(name: "Doces", order: 1)
-c2 = Category.create(name: "Queijos", order: 2)
-c3 = Category.create(name: "Carnes", order: 3)
+c1 = Category.create(name: 'Doces', order: 1)
+c2 = Category.create(name: 'Queijos', order: 2)
+c3 = Category.create(name: 'Carnes', order: 3)
 
-Product.create(name: "Doce de leite", category: c1, price: 3000)
-Product.create(name: "Doce de banana", category: c1, price: 3000)
-Product.create(name: "Doce de goiaba", category: c1, price: 3000)
-Product.create(name: "Doce de amora", category: c1, price: 3000)
+Product.create(name: 'Doce de leite', category: c1, price: 3000)
+Product.create(name: 'Doce de banana', category: c1, price: 3000)
+Product.create(name: 'Doce de goiaba', category: c1, price: 3000)
+Product.create(name: 'Doce de amora', category: c1, price: 3000)
 
-Product.create(name: "Queijo fresco", category: c2, price: 3000)
-Product.create(name: "Queijo minas", category: c2, price: 3000)
-Product.create(name: "Queijo no vinho", category: c2, price: 3000)
+Product.create(name: 'Queijo fresco', category: c2, price: 3000)
+Product.create(name: 'Queijo minas', category: c2, price: 3000)
+Product.create(name: 'Queijo no vinho', category: c2, price: 3000)
 
-Product.create(name: "Lombo suíno defumado", category: c3, price: 3000)
-Product.create(name: "Bacon", category: c3, price: 3000)
-Product.create(name: "Rosbife bovino defumado", category: c3, price: 3000)
+Product.create(name: 'Lombo suíno defumado', category: c3, price: 3000)
+Product.create(name: 'Bacon', category: c3, price: 3000)
+Product.create(name: 'Rosbife bovino defumado', category: c3, price: 3000)
