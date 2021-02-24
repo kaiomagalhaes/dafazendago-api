ce01a5069426b8c8c9f980d67128cc2c84dc1d86
viniciusaa <vini89go@gmail.com>
2021-02-24 04:27:09 +0000

    Created a route to category index

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root 'application#index'
 
   resources :products, only: :index
+  resources :categories, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
