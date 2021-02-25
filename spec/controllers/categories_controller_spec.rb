c0da88ab41e0774918f437e53bfc7d5d8459f5df
viniciusaa <vini89go@gmail.com>
2021-02-25 22:31:26 +0000

    Add 'categories' controller spec

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :controller do
+  describe "GET #index" do
+    before do
+      Category.create(name: 'Doces', order: 1)
+      Category.create(name: 'Queijos', order: 2)
+      get :index
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "JSON body response contains expected data" do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq([
+        { "name"=>"Doces", "order"=>1}, 
+        {"name"=> 'Queijos', "order"=> 2 }
+      ])
+    end
+  end
+end
