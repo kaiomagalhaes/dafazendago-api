f4c046b868a579715cae3013a754ea8adea02dad
viniciusaa <vini89go@gmail.com>
2021-02-25 23:14:02 +0000

    Updated controller specs to follow rubocop best practices

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,23 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
     before do
-      Category.create(name: "Doces", order: 1)
-      Category.create(name: "Queijos", order: 2)
+      Category.create(name: 'Doces', order: 1)
+      Category.create(name: 'Queijos', order: 2)
       get :index
     end
 
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-    
-    it "JSON body response contains expected data" do
+
+    it 'JSON body response contains expected data' do
       json_response = JSON.parse(response.body)
       expect(json_response).to eq([
-        { "name"=> "Doces", "order"=> 1 }, 
-        { "name"=> "Queijos", "order"=> 2 }
-      ])
+                                    {
+                                      'name' => 'Doces',
+                                      'order' => 1
+                                    },
+                                    {
+                                      'name' => 'Queijos',
+                                      'order' => 2
+                                    }
+                                  ])
     end
   end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
     before do
       category = Category.create(name: 'Doces', order: 1)
       Product.create(name: 'Doce de leite', category: category, price: 3000)
@@ -11,18 +11,28 @@ RSpec.describe ProductsController, type: :controller do
       get :index
     end
 
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-    
-    it "JSON body response contains expected data" do
+
+    it 'JSON body response contains expected data' do
       json_response = JSON.parse(response.body)
       expect(json_response).to eq([
-        { "name"=> "Doce de leite", "order"=> 1, "image"=> nil, "category"=> "Doces",
-          "price"=> 3000 }, 
-        { "name"=> "Doce de banana", "order"=> 1, "image"=> nil, "category"=> "Doces",
-          "price"=> 3000 }
-      ])
+                                    {
+                                      'name' => 'Doce de leite',
+                                      'order' => 1,
+                                      'image' => nil,
+                                      'category' => 'Doces',
+                                      'price' => 3000
+                                    },
+                                    {
+                                      'name' => 'Doce de banana',
+                                      'order' => 1,
+                                      'image' => nil,
+                                      'category' => 'Doces',
+                                      'price' => 3000
+                                    }
+                                  ])
     end
   end
 end
