183f674931ae0359433305eb5f0759728084dbde
viniciusaa <vini89go@gmail.com>
2021-02-26 00:58:41 +0000

    Updated controllers specs, removed 'before do' and moved the content to inside the tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -4,17 +4,16 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
   describe 'GET #index' do
-    before do
-      Category.create(name: 'Doces', order: 1)
-      Category.create(name: 'Queijos', order: 2)
-      get :index
-    end
-
     it 'returns http success' do
+      get :index
       expect(response).to have_http_status(:success)
     end
 
     it 'JSON body response contains expected data' do
+      Category.create(name: 'Doces', order: 1)
+      Category.create(name: 'Queijos', order: 2)
+      get :index
+      
       json_response = JSON.parse(response.body)
       expect(json_response).to eq([
                                     {
