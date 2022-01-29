f727e073875066e94ccbf27310afe5d946b468b5
Kaio Magalhães <me@kaiomagalhaes.com>
2022-01-29 17:18:05 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.logger = GELF::Logger.new(
-    'logs.codelitt.dev',
-    12_201,
-    'WAN',
-    { host: ENV['LOG_HOST'], environment: ENV['LOG_ENV'] }
-  )
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
